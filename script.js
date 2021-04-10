493542d56810925280dcb98b37fe1cc980a8ef18
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-04-10 18:03:09 +0200

    index.html and style.css files modified. Changed classes for buttons. script.js file modified. Added function listenShowBtns and showAnswer.

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 const $questionList = document.querySelectorAll('.question-list li'); //catch all questions from question-list
 // console.log(questionList);
 const $input = document.getElementById('search'); //catch input
-const $popup = document.getSelection('.popup'); //catch popup
-
+const $popup = document.querySelector('.popup'); //catch popup
+const $showBtns = document.querySelectorAll('.showBtn'); //catch all buttons with class showBtn
 
 const findAvailableAnswer = (e) => {
     $questionList.forEach(li => {
         if (li.textContent.toLowerCase().indexOf(e.target.value.toLowerCase())!== -1) {
-            li.style.display = 'block';
+            li.style.display = 'flex';
         } else {
             li.style.display = 'none';
         }
     });
 };
 
+const showAnswer = (e) => {
+    $popup.style.display = 'flex';
+    const liDataSet =  e.target.closest('li').getAttribute('data-num');
+    const answerContent = document.getElementById(liDataSet).innerHTML;
+    console.log(answerContent);
+    $popup.innerHTML = answerContent;
+};
+
+const listenShowBtns = () => {
+    $showBtns.forEach(btn => {
+        btn.addEventListener('click', showAnswer);
+    });
+};
+
+document.addEventListener('DOMContentLoaded', listenShowBtns);
 $input.addEventListener('keyup', findAvailableAnswer);
