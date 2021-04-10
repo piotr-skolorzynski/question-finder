b60a0a62e54f6004d8bc6eb526940fd6180bef97
Piotr Skolorzynski <p.skolorzynski@gmail.com>
2021-04-10 10:40:49 +0200

    index.html and style.css  files modified. Class search replaced by id search. script.js file modified. Function findAvailableAnswer modified

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-const $questionList = document.querySelectorAll('li'); //catch all question from question-list
+const $questionList = document.querySelectorAll('.question-list li'); //catch all question from question-list
 // console.log(questionList);
-const $input = document.getElementsByClassName('search') //catch input
-console.log($input.value);
+const $input = document.getElementById('search') //catch input
 
-// const findAvailableAnswer = () => {
-
-//     $questionList.forEach(li => {
-//         if ($input.value.toLowerCase() !== -1) {
-//             li.style.display = 'block';
-//         } else {
-//             li.style.display = 'none';
-//         }
-//     });
-// };
-
+const findAvailableAnswer = (e) => {
+    $questionList.forEach(li => {
+        if (li.textContent.toLowerCase().indexOf(e.target.value.toLowerCase())!== -1) {
+            li.style.display = 'block';
+        } else {
+            li.style.display = 'none';
+        }
+    });
+};
 
 $input.addEventListener('keyup', findAvailableAnswer);
